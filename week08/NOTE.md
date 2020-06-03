512a9ce539ae75b6a1f15d178965498efc4be87e
highcker <674636841@qq.com>
2020-06-03 22:34:45 +0800

    week08

--- week08/NOTE.md
+++ week08/NOTE.md
@@ -1,1 +1,66 @@
 # 每周总结可以写在这里
+
+# 选择器
+
+## 语法
+
+### \*
+
+### div svg|a
+
+### #id
+
+### .cls
+
+### [attr=value]
+
+### :hover
+
+### ::before
+
+### 复合选择器<简单选择器><简单选择器><简单选择器>
+
+### 复杂选择器
+
+## 优先级
+
+### 四元祖[ inlineStyle, #id, .cls, tag ]
+
+### \*, + >, ~ :not 不增加选择器的优先级
+
+### https://specifishity.com/
+
+## 伪类
+
+### 链接行为
+
+- :focus
+- :any-link
+- :link :visited
+- :hover
+- :active
+- :target
+
+### 树结构
+
+- :empty
+- :nth-child
+- :nth-last-child
+- :first-child :last-child :only-child
+
+### 逻辑型
+
+- :not
+- :where :has
+
+## 伪元素
+
+### ::before
+
+### ::after
+
+### ::first-line
+
+### ::first-letter
+
+_XMind - Trial Version_
---
+++ week08/toy-browser/render.js
@@ -0,0 +1,24 @@
+const images = require("images");
+
+function render(viewport, element) {
+  //   console.log(viewport);
+  if (element.style) {
+    const img = images(element.style.width, element.style.height);
+    if (element.style["background-color"]) {
+      console.log("[style]");
+      let color = element.style["background-color"] || "rgb(0,0,0)";
+      let c = color.match(/rgb\((\d+),(\d+),(\d+)\)/);
+      img.fill(Number(c[1]), Number(c[2]), Number(c[3]), 1);
+      viewport.draw(img, element.style.left || 0, element.style.top || 0);
+      console.log("draw element style: ", element.style);
+    }
+  }
+
+  if (element.children) {
+    for (const child of element.children) {
+      render(viewport, child);
+    }
+  }
+}
+
+module.exports = render;
